2c1863e92e344334027665d3823797bce19e2a25
JaminROCK <KingJamin@hotmail.com>
2018-03-27 21:14:54 -0600

    Update CIS2013_Week09_Homework1.cpp
    More edits

--- CIS2013_Week09_Homework1.cpp
+++ CIS2013_Week09_Homework1.cpp
@@ -1,44 +1,153 @@
-//Standard input output stream
 #include <iostream>
-//Locale for toupper
 #include <locale>
-//In case string not built in
 #include <string>
-//For input and output streams
 #include <fstream>
-//For using time as a seed
 #include <time.h>
-//Contains libraries for rng
 #include <cstdlib>
-
+#include <stdio.h>
+#include <string.h>
 using namespace std;
 
 //Functions
 int rng(int);
+void generateOTP();
+//Global Variables
 
-// c  l  d
+//User's encryption/decryption decision string
+string d;
+//Secret phrase
+char e[1000];
+//Encrypted Message
+char f[1000];
+//Locale object
+locale l;
+//Character array
+char c[27] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '};
+//Create input and output file streams
+ofstream ofs;
+ifstream ifs;
+
+char loopController = 'Y';
 
 //Main
 int main()
 {
-    //Character array
-    char c[27] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '};
-
-    //Create input and output file streams
-    ofstream ofs;
-    ifstream ifs;
-    //Create a file named otp.dat
-    ofs.open("otp.dat");
-    //Error typically happens from read or write priveleges
-    if(ofs.fail())
+    while(loopController=='Y' || loopController=='y')
     {
-        cout << "Unable to open the one time pad.";
-        exit(1);
+        //Create a file named otp.dat
+        ofs.open("otp.dat");
+        //Error typically happens from read or write privileges
+        if(ofs.fail())
+        {
+            cout << "Unable to open the one time pad.";
+            exit(1);
+        }
+
+        //Use time as a seed for the rng
+        srand(time(NULL));
+
+        generateOTP();
+
+        cout << "Would you like to \"Encrypt\" or \"Decrypt\"?: ", cin >> d, cout << endl;
+
+
+        //Loop through the d string and turn it into uppercase
+        for (int i = 0; i < d.length(); i++)
+        {
+            d[i] = toupper(d[i],l);
+        }
+        //Trick to clear cin's error flags 
+        cin.seekg(0,ios::end);
+        cin.clear();
+
+
+        
+        //If the user types in encrypt
+        if(d == "ENCRYPT")
+        {
+            cout << "Type in what you want to encrypt on the following line. . .\n";
+            
+            //Receive input
+            cin.getline(e, 1000);
+            //Get length
+            int length = strlen(e);
+            //Open the otp
+            ifs.open("otp.dat");
+            if(ifs.fail())
+            {
+                cout << "There was an error opening the file.";
+                exit(1);
+            }
+            //Glean rotation key from the otpKey
+            char otpKey;
+            int rotationKey[length];
+            for(int i = 0; i < length; i++)
+            {
+                ifs >> otpKey;
+                for(int j = 0; j < 27; j++)
+                {
+                    if(otpKey == c[j])
+                    {
+                        cout << i << " - " << otpKey << " : " << j << endl;
+                        rotationKey[i] = j;
+                    }
+                }
+            }
+
+            cout << "-------------" << endl;
+
+            //Glean current key
+            int currentKey[length];
+            for(int i = 0; i < length; i++)
+            {
+                for(int j = 0; j < 27; j++)
+                {
+                    if(e[i] == c[j])
+                    {
+                        cout << i << " - " << e[i] << " : " << j << endl;
+                        currentKey[i] = j;
+                    }
+                }
+            }
+
+            cout << "-------------" << endl;
+
+            //Create final string for encrypted message
+
+            for(int i = 0; i < length; i++)
+            {
+                int newKey = currentKey[i] + rotationKey[i];
+                newKey = newKey % 27;
+                cout << i << " - " << c[newKey] << endl;
+            }
+
+        }
+        //If the user types in decrypt
+        if(d == "DECRYPT")
+        {
+            cout << "Hey";
+        }
+
+        cout << "Would you like to continue? (y/n): ", cin >> loopController, cout << endl;
+        //Close the file streams
+        ofs.close();
+        ifs.close();
     }
 
-    //Use time as a seed for the rng
-    srand(time(NULL));
+    return 0;
+}
 
+
+
+//Function that generates a random number
+int rng(int max)
+{
+    int randomN = 1 + rand() % max;
+    return randomN;
+}
+
+void generateOTP()
+{
     //Generates the One Time Pad file
     //Ten lines
     for(int i = 0; i < 10; i++)
@@ -51,88 +160,4 @@ int main()
         }
         ofs << endl;
     }
-    string d;
-    cout << "Would you like to \"Encrypt\" or \"Decrypt\"?: ", cin >> d, cout << endl;
-
-    //Create a locale object
-    locale l;
-    //Loop through the d string and turn it into uppercase
-    for (int i = 0; i < d.length(); i++)
-    {
-        d[i] = toupper(d[i],l);
-    }
-
-    string e;
-
-    //If the user types in encrypt
-    if(d == "ENCRYPT")
-    {
-        cout << "Type in what you want to encrypt on the following line. . .\n", cin >> e;
-        //Open the otp
-        ifs.open("otp.dat");
-        if(ifs.fail())
-        {
-            cout << "There was an error opening the file.";
-            exit(1);
-        }
-        //Glean rotation key from the otpKey
-        char otpKey;
-        int rotationKey[e.length()];
-        for(int i = 0; i < e.length(); i++)
-        {
-            ifs >> otpKey;
-            for(int j = 0; j < 27; j++)
-            {
-                if(otpKey == c[j])
-                {
-                    cout << otpKey << " : " << j << endl;
-                    rotationKey[i] = j;
-                }
-            }
-        }
-
-        cout << "-------------" << endl;
-
-        //Glean current key
-        int currentKey[e.length()];
-        for(int i = 0; i < e.length(); i++)
-        {
-            for(int j = 0; j < 27; j++)
-            {
-                if(e[i] == c[j])
-                {
-                    cout << e[i] << " : " << j << endl;
-                    currentKey[i] = j;
-                }
-            }
-        }
-
-        //Create final string for encrypted message
-        string f;
-
-
-    }
-    //If the user types in decrypt
-    if(d == "DECRYPT")
-    {
-        cout << "Hey";
-    }
-
-
-
-
-
-
-
-
-    return 0;
-}
-
-
-
-//Function that generates a random number
-int rng(int max)
-{
-    int randomN = 1 + rand() % max;
-    return randomN;
 }
